dfacc2060a889a795e0cb11df6d0ff9f13ef1a03
Saiteja5533-hub <saitejailoni@gmail.com>
2025-02-19 02:11:53 -0500

    h

--- s3.py
+++ s3.py
@@ -1,1 +1,6 @@
 import boto3
+session = boto3.Session(
+    aws_access_key_id="AKIAS74TMAZM7ZTJZUUW",
+    aws_secret_access_key="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",
+    region_name="US East (N. Virginia) us-east-1",
+)
